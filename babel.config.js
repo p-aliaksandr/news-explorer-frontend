4583fd2eecb45844d231133adaf8a13df86fe743
Sasha Prokofyev <ablehope@yandex.by>
2020-02-02 21:21:22 +0300

    fixed problems

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 const presets = [
   [
-    "@babel/env",
+    '@babel/env',
     {
       targets: { // указать цели, для полифилов
-            android: '67',
-            ios: '12',
-            edge: "17",
-            ie: "11",
-            firefox: "50",
-            chrome: "64",
-            safari: "11.1",
+        esmodules: true,
+        android: '67',
+        ios: '12',
+        edge: '17',
+        firefox: '60',
+        chrome: '64',
+        safari: '11.1',
       },
-      useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-      corejs: "3.4.1" // явно проставить версию corejs
-    }
+      useBuiltIns: 'usage', // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
+      corejs: '3.1.4', // явно проставить версию corejs
+    },
   ],
 ];
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const webpack = require('webpack');
+const cssnano = require('cssnano');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+      main: './src/index.js',
+      articles: './src/saved-articles.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
@@ -17,7 +25,11 @@ module: {
             },
             {
               test: /\.css$/, // применять это правило только к CSS-файлам
-              use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+              use: [
+                isDev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } },
+                'css-loader',
+                'postcss-loader',
+              ] // к этим файлам нужно применить пакеты, которые мы уже установили
             },
 
             {
@@ -26,15 +38,32 @@ module: {
                 'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
                 {
                   loader: 'image-webpack-loader',
-                  options: {},
+                  options: {
+                    mozjpeg: {
+                      progressive: true,
+                      quality: 85,
+                  },
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [
+                      0.7,
+                      0.9,
+                    ],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
                 },
-              ],
-            },
-            {
-              test: /\.(eot|ttf|woff|woff2)$/,
-              loader: 'file-loader?name=./vendor/[name].[ext]',
-            },
-
+              },
+            ],
+          },
+          {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=./vendor/[name].[ext]',
+          },
         ]
     },
     plugins: [
@@ -55,6 +84,19 @@ module: {
         template: './src/saved-articles.html', // откуда брать образец для сравнения с текущим видом проекта
         filename: 'saved-articles.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
       }),
-      new WebpackMd5Hash()
-  ]
+      new WebpackMd5Hash(),
+      new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      }),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: cssnano,
+        cssProcessorPluginOptions: {
+          preset: [
+            'default',
+          ],
+        },
+        canPrint: true,
+      }),
+  ],
 };
