7236d08d37698de11f0e4d3d164c0276cc5bf96d
Sasha Prokofyev <ablehope@yandex.by>
2020-02-03 22:35:21 +0300

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Подключили к проекту плагин
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const webpack = require('webpack');
@@ -9,94 +9,94 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    entry: {
-      main: './src/index.js',
-      articles: './src/saved-articles.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-        },
-module: {
+  entry: {
+    main: './src/index.js',
+    articles: './src/saved-articles.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
     rules: [{ // тут описываются правила
-        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
-        exclude: /node_modules/ // исключает папку node_modules
-            },
-            {
-              test: /\.css$/, // применять это правило только к CSS-файлам
-              use: [
-                isDev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } },
-                'css-loader',
-                'postcss-loader',
-              ] // к этим файлам нужно применить пакеты, которые мы уже установили
-            },
-
-            {
-              test: /\.(png|jpg|gif|ico|svg)$/,
-              use: [
-                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
-                {
-                  loader: 'image-webpack-loader',
-                  options: {
-                    mozjpeg: {
-                      progressive: true,
-                      quality: 85,
-                  },
-                  optipng: {
-                    enabled: false,
-                  },
-                  pngquant: {
-                    quality: [
-                      0.7,
-                      0.9,
-                    ],
-                    speed: 4,
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=./vendor/[name].[ext]',
-          },
-        ]
+      test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+      use: { loader: 'babel-loader' }, // весь JS обрабатывается пакетом babel-loader
+      exclude: /node_modules/, // исключает папку node_modules
     },
-    plugins: [
-      new MiniCssExtractPlugin({
-          filename: 'style.[contenthash].css'
-      }),
-      new HtmlWebpackPlugin({
-        // Означает, что:
-        inject: false, // стили НЕ нужно прописывать внутри тегов
-        hash: true, // для страницы нужно считать хеш
-        template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
-        filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-      }),
-      new HtmlWebpackPlugin({
-        // Означает, что:
-        inject: false, // стили НЕ нужно прописывать внутри тегов
-        hash: true, // для страницы нужно считать хеш
-        template: './src/saved-articles.html', // откуда брать образец для сравнения с текущим видом проекта
-        filename: 'saved-articles.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-      }),
-      new WebpackMd5Hash(),
-      new webpack.DefinePlugin({
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      }),
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: cssnano,
-        cssProcessorPluginOptions: {
-          preset: [
-            'default',
-          ],
+    {
+      test: /\.css$/, // применять это правило только к CSS-файлам
+      use: [
+        isDev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader, options: { publicPath: './' } },
+        'css-loader',
+        'postcss-loader',
+      ], // к этим файлам нужно применить пакеты, которые мы уже установили
+    },
+
+    {
+      test: /\.(png|jpg|gif|ico|svg)$/,
+      use: [
+        'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 85,
+            },
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [
+                0.7,
+                0.9,
+              ],
+              speed: 4,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+          },
         },
-        canPrint: true,
-      }),
+      ],
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]',
+    },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      hash: true, // для страницы нужно считать хеш
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html', // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    }),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      hash: true, // для страницы нужно считать хеш
+      template: './src/saved-articles.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'saved-articles.html', // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+        ],
+      },
+      canPrint: true,
+    }),
   ],
 };
