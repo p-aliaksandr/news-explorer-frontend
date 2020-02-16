5d56d51b2f8ad53ef4fc9ca0200325199905032f
Sasha Prokofyev <ablehope@yandex.by>
2020-02-16 18:50:54 +0300

    added open and close mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './pages/style.css';
+
+const buttonMobileMenu = document.querySelector('.navigation__menu-mobile');
+const crossMobileMenu = document.querySelector('.popup__menu-close');
+
+function openMobileMenu() {
+  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+}
+
+function closeMobileMenu() {
+  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+}
+
+buttonMobileMenu.addEventListener('click', openMobileMenu);
+crossMobileMenu.addEventListener('click', closeMobileMenu);
